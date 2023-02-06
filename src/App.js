642bc8f943fcd350c07e8e0264d7fb9fb71b3408
paul bratslavsky <paulbratslavsky@pauls-Mac-mini.local>
2023-02-06 17:26:46 -0600

    highlight component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import SaveSelectedText from './components/SaveSelectedText';
 
-function App() {
+function App() { 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SaveSelectedText >
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in elit a nisl ullamcorper egestas. Aliquam pretium neque ac aliquam tristique. Aliquam euismod tincidunt metus, vestibulum sodales erat. Sed interdum feugiat nisl, ac aliquet est porta in. Phasellus sit amet odio magna. Nunc porttitor nec libero eget sagittis. Aenean sit amet ultrices mauris. Sed sit amet tempus nibh. Ut quis accumsan neque. Donec nec sollicitudin mi. Proin bibendum ultricies magna nec mattis. Suspendisse vel sollicitudin risus. Nulla dictum volutpat erat, at tempus libero vulputate ut.</p>
+        <p>Morbi eros urna, venenatis quis egestas sit amet, ultrices id nisi. Etiam quis suscipit velit, a volutpat lectus. Proin tincidunt massa sit amet nisl tincidunt, quis tincidunt dui pellentesque. Nulla vitae lacus sit amet mauris eleifend interdum. Ut nec felis at elit blandit pulvinar at sed ante. Integer gravida dolor ut facilisis condimentum. Sed at justo sem. Quisque quis pulvinar ligula, quis ullamcorper augue. Duis posuere, ante sed venenatis vehicula, tellus dui consectetur tellus, nec tempor neque quam in nunc. Duis ut sem mi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus dapibus ornare placerat.</p>
+        <p>Sed nec nulla in augue gravida porttitor. Praesent pulvinar enim ac dolor pellentesque ullamcorper. Sed erat augue, ultricies et diam eu, dapibus tincidunt ante. Cras libero magna, convallis nec massa quis, varius interdum risus. Integer nibh sapien, semper ut nunc quis, consectetur placerat augue. Sed dignissim iaculis risus. Vestibulum scelerisque convallis orci vel ultrices. Maecenas imperdiet, lacus eu efficitur consequat, ex ex scelerisque elit, sit amet commodo dolor nunc ac erat. In pulvinar turpis ac lorem convallis, vitae congue nulla aliquet. Vivamus vitae tincidunt nisl, tempor consectetur odio. Mauris facilisis, lorem non mattis vestibulum, turpis arcu tempor dolor, vitae placerat enim dolor in dui. Sed imperdiet faucibus nisl, sed sodales urna tincidunt at. Cras libero est, venenatis eu massa molestie, tempor ullamcorper eros. Donec dignissim, orci et pellentesque mattis, lectus elit blandit tortor, eget ultrices felis nunc ac justo. Suspendisse vel lectus scelerisque, lobortis eros ut, molestie velit.</p>
+      </SaveSelectedText>
     </div>
   );
 }
---
+++ src/components/SaveSelectedText.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Column = styled.div`
+  flex: 1;
+  padding: 1rem;
+`;
+
+const SelectionList = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const SelectionItem = styled.li`
+  margin: 1rem 0;
+`;
+
+const SaveButton = styled.button`
+  margin-top: 1rem;
+  padding: 0.5rem 1rem;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+function Selections({ selections }) {
+  return (
+    <SelectionList>
+      {selections.map((selection, index) => {
+        return <SelectionItem key={index}>{selection}</SelectionItem>;
+      })}
+    </SelectionList>
+  );
+}
+
+const SaveSelectedText = ({ children }) => {
+  const [selectedText, setSelectedText] = useState("");
+  const [selections, setSelections] = useState([]);
+
+  useEffect(() => {
+    document.addEventListener("mouseup", handleSelection);
+    return () => {
+      document.removeEventListener("mouseup", handleSelection);
+    };
+  }, []);
+
+  const handleSelection = () => {
+    let selection = window.getSelection().toString();
+    if (selection) {
+      setSelectedText(selection);
+      setSelections((prevState) => [...prevState, selection]);
+    }
+  };
+
+  const handleSaveNote = async () => {
+    selections.forEach(async (selection) => {
+      const response = await fetch("http://localhost:1337/api/notes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ data: { text: selection } }),
+      });
+
+      const result = await response.json();
+      console.log(result);
+    });
+  };
+
+  return (
+    <Container>
+      <Column>
+        <p>Current Selection: {selectedText}</p>
+        <div>{children && children}</div>
+      </Column>
+      <Column>
+        <Selections selections={selections} />
+        <SaveButton onClick={handleSaveNote}>Save Note</SaveButton>
+      </Column>
+    </Container>
+  );
+};
+
+export default SaveSelectedText;
